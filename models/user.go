e50233ef19071181aa34dd57ae9d45417c576e02
Jim Shaw <jimxshaw@gmail.com>
2019-08-13 21:55:22 -0500

    Add GetUsers and AddUser methods in models package.

--- models/user.go
+++ models/user.go
@@ -1,5 +1,6 @@
 package models
 
+// User is the default user object.
 type User struct {
 	ID        int
 	FirstName string
@@ -10,3 +11,16 @@ var (
 	users  []*User
 	nextID = 1
 )
+
+// GetUsers will retrieve a slice of users.
+func GetUsers() []*User {
+	return users
+}
+
+// AddUser adds a user to the users slice.
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+	return u, nil
+}
