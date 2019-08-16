c47b6b7c46e6de3cd397cfa9dd583b5057a9da69
Jim Shaw <jimxshaw@gmail.com>
2019-08-15 22:34:52 -0500

    Add constructor function with user controller.

--- controllers/user.go
+++ controllers/user.go
@@ -1,9 +1,25 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type userController struct{}
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
 
+// newUserController is a constructor function that
+// returns a newly configured user controller.
+func newUserController() *userController {
+	// It is permissible to immediately use an address-of
+	// operator on newly created structs.
+	return &userController{
+		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`),
+	}
+}
+
+// ServeHTTP takes in a request and returns a response.
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the User Controller!"))
 }
